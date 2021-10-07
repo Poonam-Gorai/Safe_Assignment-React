907e358afcaa31895ff873171ca559e981b6c3d1
Poonam-Gorai <poonamgorai1807@gmail.com>
2021-10-07 14:17:49 +0530

    commit

---
+++ src/pages/Sales/button/Button.js
@@ -0,0 +1,36 @@
+import { useState } from "react";
+import Backdrop from '../backdrop/Backdrop';
+import Modal from '../modal/Modal';
+import "./Button.css";
+
+function Button() {
+  
+  const [modalIsOpen, setModalIsOpen] = useState(false);
+
+  function ModalHandler() {
+    setModalIsOpen(true);
+
+    //console.log('Clicked');
+  }
+  function closeModalHandler() {
+    setModalIsOpen(false);
+  }
+
+
+  return (
+    <div className='btn-size'>
+      
+      <button className="btn" onClick={ModalHandler}>
+        <a className="plus">+</a>
+      </button>
+      {modalIsOpen && (
+        <Modal
+          onCancel={closeModalHandler}
+        />
+      )}
+      {modalIsOpen && <Backdrop onClick={closeModalHandler} />}
+    </div>
+  );
+}
+
+export default Button;
