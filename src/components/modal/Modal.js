39c9be64a6d2df5450eb060696c3799a28415180
Poonam-Gorai <poonamgorai1807@gmail.com>
2021-10-18 16:48:43 +0530

    commit

--- src/components/modal/Modal.js
+++ src/components/modal/Modal.js
@@ -1,4 +1,4 @@
-import { useRef } from "react";//for reading the user input using reference
+import { useRef } from "react"; //for reading the user input using reference
 import "./Modal.css";
 
 
@@ -7,8 +7,6 @@ import {createSafe} from '../../redux/createSafe/createSafe.action';
 
 
 function Modal(props) {
-
-  
   const dispatch = useDispatch();
 
   //it is like a useState only and it preserve the value. No rerender
@@ -17,43 +15,46 @@ function Modal(props) {
   const typeInputRef = useRef();
   const descriptionInputRef = useRef();
 
-    function cancleHandler(){
-        props.onCancel();
+  function cancleHandler() {
+    props.onCancel();
+  }
+
+  function submitHandler(event) {
+    event.preventDefault(); //to prevent Default input
+
+    //it holds the currently added input //this is a javascript property
+    const enteredSafeName = safeNameInputRef.current.value;
+    const enteredOwner = ownerInputRef.current.value;
+    const enteredType = typeInputRef.current.value;
+    const enteredDescription = descriptionInputRef.current.value;
+
+    if (
+      !enteredSafeName ||
+      !enteredOwner ||
+      !enteredType ||
+      !enteredDescription
+    ) {
+      return alert("Please fill in all the fileds!");
+      //return toast.warn("Please fill in all the fileds!");
     }
-
-    function submitHandler(event){
-
-      event.preventDefault();//to prevent Default input 
-
-      //it holds the currently added input //this is a javascript property
-      const enteredSafeName = safeNameInputRef.current.value;
-      const enteredOwner = ownerInputRef.current.value;
-      const enteredType = typeInputRef.current.value;
-      const enteredDescription = descriptionInputRef.current.value;
-
-      if (!enteredSafeName || !enteredOwner || !enteredType || !enteredDescription){
-        return alert("Please fill in all the fileds!")
-        //return toast.warn("Please fill in all the fileds!");
-      }
-      const modalData = {
-        safename: enteredSafeName,
-        owner:  enteredOwner,
-        type: enteredType,
-        description: enteredDescription,
-        secrets:[]
-      };
-      //console.log(modalData);
+    const modalData = {
+      safename: enteredSafeName,
+      owner: enteredOwner,
+      type: enteredType,
+      description: enteredDescription,
+      secrets: [],
+    };
+    //console.log(modalData);
 
       dispatch(createSafe(modalData));
       cancleHandler();
     }
 
-
   return (
-    <form className="modal" >
+    <form className="modal">
       <p className="create">Create Safe</p>
       <div className="modal-img">
-        <img src="./assets/icon.png" className="icon"alt='iconimg'></img>
+        <img src="./assets/icon.png" className="icon" alt="iconimg"></img>
         <p className="model-content">
           A Safe is a logical unit to store the secrets. All the safes are
           created within Vault. You can control access only at the safe level.
@@ -61,24 +62,67 @@ function Modal(props) {
           content of the safe.
         </p>
       </div>
-      <div className='input-content'>
-      <span className='Safename' htmlFor='safename'>Safe Name</span>
-      <input type="text" placeholder="Safe Name" id='safename'className="Safe" ref={safeNameInputRef} autoComplete='off' required></input>
-      <span className='Safename' htmlFor='owner'>Owner</span>
-      <input type="text" placeholder="Owner"id='owner' className="Safe" ref={ownerInputRef} autoComplete='off' required></input>
-      <span className='Safename'htmlFor='type'>Type</span>
-      <input type="text" placeholder="Type" id='type'className="Safe" ref={typeInputRef} autoComplete='off' required></input>
-      <span className='Safename'htmlFor='description'>Description</span>
-      <textarea placeholder="Description"id='description' className="Safe" rows='3' ref={descriptionInputRef} required></textarea>
-      <span className='Safename'>Please add minimum of 10 characters</span>
+      <div className="input-content">
+        <span className="Safename" htmlFor="safename">
+          Safe Name
+        </span>
+        <input
+          type="text"
+          placeholder="Safe Name"
+          id="safename"
+          className="Safe"
+          ref={safeNameInputRef}
+          autoComplete="off"
+          required
+        ></input>
+        <span className="Safename" htmlFor="owner">
+          Owner
+        </span>
+        <input
+          type="text"
+          placeholder="Owner"
+          id="owner"
+          className="Safe"
+          ref={ownerInputRef}
+          autoComplete="off"
+          required
+        ></input>
+        <span className="Safename" htmlFor="type">
+          Type
+        </span>
+        <input
+          type="text"
+          placeholder="Type"
+          id="type"
+          className="Safe"
+          ref={typeInputRef}
+          autoComplete="off"
+          required
+        ></input>
+        <span className="Safename" htmlFor="description">
+          Description
+        </span>
+        <textarea
+          placeholder="Description"
+          id="description"
+          className="Safe"
+          rows="3"
+          ref={descriptionInputRef}
+          required
+        ></textarea>
+        <span className="Safename">Please add minimum of 10 characters</span>
       </div>
-      <div className='modal-btn'>
-      <button className="modal-btn-cancle"onClick={cancleHandler} >Cancel</button>
-      <button className="modal-btn-create"onClick={submitHandler} > + Create</button>
+      <div className="modal-btn">
+        <button className="modal-btn-cancle" onClick={cancleHandler}>
+          Cancel
+        </button>
+        <button className="modal-btn-create" onClick={submitHandler}>
+          {" "}
+          + Create
+        </button>
       </div>
     </form>
   );
 }
 
 export default Modal;
- 
